b24a19646326bc579a0f8a3e0cee45eadc9c4690
Lorennabarboza16 <lorenna.barboza16@gmail.com>
2024-11-14 08:03:42 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,4 +1,3 @@
-
 criaCartao(
     'Programação',
     'O que é Python?',
@@ -15,4 +14,10 @@ criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
